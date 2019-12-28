2d3588f308bb5b6b6b9def9c4621185a1b856ca5
Kelly M Collins <90kcol@gmail.com>
2019-12-28 14:20:10 -0500

    added controllers and routes

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 const express = require('express')
 // Add variable to express
 const app = express();
+const StudioRoutes = require('./lib/routes/StudioGhibli')
 
 
 // Make sure the library is listening
 // .listen() - application will wait for requests from a specific port
 app.listen(8080, () => {
     console.log('I am listening.....');
-    
-})
+});
+
+// When user requests at the root / of our application, .use references a particular router
+app.use('/', StudioRoutes);
